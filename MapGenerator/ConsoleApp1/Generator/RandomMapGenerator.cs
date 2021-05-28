6633c540fc10b52e62c92d254ce0f223b6f9b09e
daichihasegawa0923 <punyachi@yahoo.co.jp>
2021-05-28 18:00:12 +0900

    add project files

---
+++ MapGenerator/ConsoleApp1/Generator/RandomMapGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapGenerator.Code.Generator
+{
+    public class RandomMapGenerator
+    {
+        public static int[,] Generate(int x, int y)
+        {
+            var map = new int[y, x];
+
+            var isX = new Random().Next(0, 1) == 0;
+
+            var startPoint = new Point(
+                isX ? new Random().Next(0, map.GetLength(1) - 1) : 0,
+                !isX ? new Random().Next(0, map.GetLength(0) - 1) : 0);
+
+            var Xcount = isX ? startPoint.x : 0;
+            var Ycount = !isX ? startPoint.y : 0;
+
+            var dividions = new List<int[][]>();
+
+
+            var divide = new int[isX ? map.GetLength(0) : map.GetLength(1)][];
+
+            for (var i = 0; i < (isX ? map.GetLength(0):map.GetLength(1)); i++)
+            {
+                map[Ycount, Xcount] = 1;
+                divide[isX?Ycount:Xcount] = new int[]{Ycount,Xcount};
+
+                if (isX)
+                    Ycount++;
+                else
+                    Xcount++;
+            }
+
+            dividions.Add(divide);
+
+            return map;
+        }
+
+        class Point
+        {
+            public readonly int x;
+            public readonly int y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+    }
+}
---
+++ MapGenerator/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MapGenerator.Code.Generator;
+
+namespace MapGenerator.Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = RandomMapGenerator.Generate(10, 10);
+            for (var i = 0; i < map.GetLength(0); i++)
+            {
+                for(var j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
